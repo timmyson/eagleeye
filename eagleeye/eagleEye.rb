af20bbc432aa6d98eb11d85260dc4f863adf5d9c
Tim Foster Gusba <tifoster@akamai.com>
2015-05-06 17:20:15 -0400

    Quick broad architecture

--- eagleeye/eagleEye.rb
+++ eagleeye/eagleEye.rb
@@ -1,4 +1,14 @@
 require_relative "./imageDict.rb"
 include ImageDictionaryModule
 
+#read list of URL's
 
+#run script.rb (better name pending) to scrape URL for images
+#
+#fetch various output formats
+#
+#fetch image as requested by each browser
+#
+#find size and format we think the best should be
+#
+#compare file size and format between the two, raise error on any discrepency
